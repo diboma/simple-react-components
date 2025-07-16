f1af4adaca32d0289b202eef298b240d7a369410
diboma <diboma@gmail.com>
2025-07-16 14:52:51 +0200

    Ladle: update build

--- src/stories/_github.stories.tsx
+++ src/stories/_github.stories.tsx
@@ -1,16 +1,18 @@
-// export const meta = {
-//   title: '🔗 GitHub Repository',
-// }
+import { containerStyles } from './utils/styles'
 
 export const GitHubStory = () => (
-  <a
-    href="https://github.com/diboma/simple-react-components"
-    target="_blank"
-    rel="noopener noreferrer"
-    style={{ fontSize: '1.2rem', padding: '1rem', display: 'block' }}
-  >
-    👉 Bekijk de repository op GitHub
-  </a>
+  <main style={containerStyles}>
+    <h1>Simple React Components</h1>
+
+    <a
+      href="https://github.com/diboma/simple-react-components"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ fontSize: '1.2rem', padding: '1rem', display: 'block' }}
+    >
+      👉 View the repository on GitHub
+    </a>
+  </main>
 )
 
 GitHubStory.storyName = 'Github Repository'
