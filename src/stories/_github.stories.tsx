96ea507601e3190f39d29a056220a9c5a476ae99
diboma <diboma@gmail.com>
2025-07-16 14:33:48 +0200

    Ladle: add link to GitHub Repository

---
+++ src/stories/_github.stories.tsx
@@ -0,0 +1,16 @@
+// export const meta = {
+//   title: '🔗 GitHub Repository',
+// }
+
+export const GitHubStory = () => (
+  <a
+    href="https://github.com/diboma/simple-react-components"
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{ fontSize: '1.2rem', padding: '1rem', display: 'block' }}
+  >
+    👉 Bekijk de repository op GitHub
+  </a>
+)
+
+GitHubStory.storyName = 'Github Repository'
